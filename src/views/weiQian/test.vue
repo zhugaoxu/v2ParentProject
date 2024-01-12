efad3fe4aa03e89013d3470edccf0362d6758ee5
zhugaoxu <15037270681@163.com>
2024-01-12 17:25:38 +0800

    提交

--- src/views/weiQian/test.vue
+++ src/views/weiQian/test.vue
@@ -2,7 +2,7 @@
   <div>
     <micro-app
       name="shae"
-      url="http://10.10.10.219:8081/"
+      url="http://43.139.16.124:8087/"
       baseroute="/wei/testVueTwo"
     ></micro-app>
   </div>
